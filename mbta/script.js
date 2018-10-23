9b157bc081434c6308b0ca39998b52a2927aed9b
Colby44 <colbyazersky@gmail.com>
2018-10-22 21:37:12 -0400

    Final working version of all files

--- mbta/script.js
+++ mbta/script.js
@@ -13,25 +13,25 @@ var map;
 var marker;
 var infowindow = new google.maps.InfoWindow();
 var tStops = [
-  ['Alewife', 42.395428, -71.142483, 'place-alfcl'],
-  ['Davis', 42.39674, -71.121815, 'place-davis'],
-  ['Porter Square', 42.3884, -71.11914899999999, 'place-portr'],
-  ['Harvard Square', 42.373362, -71.118956, 'place-harsq'],
-  ['Central Square', 42.365486 , -71.103802, 'place-cntsq'],
-  ['Kendall/MIT', 42.36249079, -71.08617653, 'place-knncl'],
-  ['Charles/MGH', 42.361166, -71.070628, 'place-chmnl'],
-  ['Park Street', 42.35639457, -71.0624242, 'place-pktrm'],
-  ['Downtown Crossing', 42.355518, -71.060225, 'place-dwnxg'],
-  ['South Station', 42.352271, -71.05524200000001, 'place-sstat'],  
-  ['Broadway', 42.342622, -71.056967, 'place-brdwy'],
-  ['Andrew', 42.330154 , -71.057655, 'place-andrw'],
-  ['JFK/UMass', 42.320685, -71.052391, 'place-jfk'],
-  ['Salvin Hill', 42.31129, -71.053331, 'place-shmnl'],
-  ['Fields Corner', 42.300093, -71.061667, 'place-fldcr'],
-  ['Ashmont', 42.284652, -71.06448899999999, 'place-asmnl'],
-  ['Wollaston', 42.2665139, -71.0203369, 'place-wlsta' ],
-  ['Quincy Center', 42.251809, -71.005409, 'place-qnctr' ],
-  ['Braintree', 42.2078543, -71.0011385, 'place-brntn']
+['Alewife', 42.395428, -71.142483, 'place-alfcl'],
+['Davis', 42.39674, -71.121815, 'place-davis'],
+['Porter Square', 42.3884, -71.11914899999999, 'place-portr'],
+['Harvard Square', 42.373362, -71.118956, 'place-harsq'],
+['Central Square', 42.365486 , -71.103802, 'place-cntsq'],
+['Kendall/MIT', 42.36249079, -71.08617653, 'place-knncl'],
+['Charles/MGH', 42.361166, -71.070628, 'place-chmnl'],
+['Park Street', 42.35639457, -71.0624242, 'place-pktrm'],
+['Downtown Crossing', 42.355518, -71.060225, 'place-dwnxg'],
+['South Station', 42.352271, -71.05524200000001, 'place-sstat'],  
+['Broadway', 42.342622, -71.056967, 'place-brdwy'],
+['Andrew', 42.330154 , -71.057655, 'place-andrw'],
+['JFK/UMass', 42.320685, -71.052391, 'place-jfk'],
+['Salvin Hill', 42.31129, -71.053331, 'place-shmnl'],
+['Fields Corner', 42.300093, -71.061667, 'place-fldcr'],
+['Ashmont', 42.284652, -71.06448899999999, 'place-asmnl'],
+['Wollaston', 42.2665139, -71.0203369, 'place-wlsta' ],
+['Quincy Center', 42.251809, -71.005409, 'place-qnctr' ],
+['Braintree', 42.2078543, -71.0011385, 'place-brntn']
 ];
 var image = {
   url: 'https://pbs.twimg.com/profile_images/479253963217186816/gh7IsaeA_400x400.jpeg',
@@ -50,11 +50,10 @@ function init() {
       me = new google.maps.LatLng(myLat, myLng);
       map.panTo(me);   
       renderMapLocationBased();
-      getSchedule();
     },   function() { noGeoLocationError(true);
     });
   } else {
-      noGeoLocationError(false);
+    noGeoLocationError(false);
   }
 
 }
@@ -81,7 +80,7 @@ function drawLine() {
     geodesic: true,
     strokeColor: '#FF0000',
     strokeOpacity: 1.0,
-     strokeWeight: 5
+    strokeWeight: 5
   });
   flightPath.setMap(map);
 
@@ -100,21 +99,11 @@ function drawLine() {
     geodesic: true,
     strokeColor: '#FF0000',
     strokeOpacity: 1.0,
-     strokeWeight: 5
+    strokeWeight: 5
   });
   flightPath2.setMap(map);
 }
 
-function clickTitle(marker){
-    marker.addListener('click', function() {
-
-    infowindow.setPosition(marker.getPosition());
-    marker.title = 
-  infowindow.setContent(marker.title);
-    infowindow.open(map, marker);
-    map.setCenter(marker.getPosition());
-    });
-}
 
 
 function makeTStopTitles(){
@@ -133,55 +122,66 @@ function makeTStopTitles(){
 
 
 function clickAbleStop(){
-  markerArray[0].addListener('click', function() {
-    marker = markerArray[0];
-        linkOfRequest = 'https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id='+ tStops[0][3];
-        request = new XMLHttpRequest();
-        request.open("GET", linkOfRequest, true);
-        request.send();
-        request.onreadystatechange = function() {
-      if (request.readyState == 4 && request.status == 200) {
-          var data = request.responseText;
-          var parsed = (JSON.parse(data));
- 
-
-          var parsedData = parsed.data;
-          console.log (parsedData.length);
+  for (var j = 0; j < markerArray.length; j++) {
+    markerArray[j].content = ""; 
+    markerArray[j].index = j; 
+    var marker = markerArray[j];
+    marker.addListener('click', function() {
+      linkOfRequest = 'https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id='+ tStops[this.index][3];
+      var request = new XMLHttpRequest();
+      request.index = this.index;
+      request.open("GET", linkOfRequest, true);
+      request.send();
+      request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+          var parsedData = JSON.parse(request.response);
+          var dataArray = parsedData.data;
           var scheduleStr="";
           var pos;
-      for (var i = 0; i<parsedData.length;i++) {
-          var  dir = parsedData[i].attributes.direction_id;
-          var arriveString = parsedData[i].attributes.arrival_time;
-
-          if (dir==1)
-          {
-            direction = "Southbound";
+          if (dataArray == null){
+            alert("Too many requests! Please wait a second and try again...");
           }
-          if (dir==0)
-          {
-            direction = "Northbound";
+          for (var i = 0; i<dataArray.length-1;i++) {
+            var  dir = dataArray[i].attributes.direction_id;
+            var arrive =  dataArray[i].attributes.arrival_time;
+            var depart =  dataArray[i].attributes.departure_time;  
+            var arriveString;
+            if (arrive == null & depart == null){
+              arriveString = "N/A";
+            } else if (arrive == null & depart != null){
+              arriveString = depart;
+            } else if (arrive != null & depart == null){
+              arriveString = arrive; 
+            } else if (arrive !=null & depart != null){
+              arriveString = depart;
+            }
+            if (dir==1){
+              direction = "Southbound";
+            }
+            if (dir==0){
+              direction = "Northbound";
+            }
+            var posSplice = arriveString.indexOf("T");
+            arriveString = arriveString.slice(-(arriveString.length-posSplice-1));
+            arriveString = arriveString.slice(0,arriveString.length-6);
+            scheduleStr =  scheduleStr + "&nbsp" + arriveString + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" + direction + "</br>";
           }
-          var posSplice = arriveString.indexOf("T");
-          arriveString = arriveString.slice(-(arriveString.length-posSplice-1));
-          arriveString = arriveString.slice(0,arriveString.length-6);
-          scheduleStr =  scheduleStr + "&nbsp" + arriveString + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" + direction + "</br>";
-      }
-       scheduleStr = "<u>Time of Arrival</u> &nbsp &nbsp &nbsp &nbsp <u>Direction</u></br>" + scheduleStr;
+          scheduleStr = "<u>Time</u> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <u>Direction</u></br>" + scheduleStr;
 
-          infowindow.setPosition(marker.getPosition());
-          infowindow.setContent("This stop is: <b>" + tStops[0][0] + "</b>. </br> Here is the upcoming schedule: <br> <br>" + scheduleStr);
-          infowindow.open(map, marker);
-          map.setCenter(marker.getPosition());
+          var displayStr = "This stop is: <b>" + tStops[markerArray[this.index].index][0] + "</b>. </br> Here is the upcoming schedule: <br> <br>" + scheduleStr;
+          infowindow.setPosition(markerArray[this.index].getPosition());
+          infowindow.setContent(displayStr);
+          infowindow.open(map, markerArray[this.index]);
+          map.setCenter(markerArray[this.index].getPosition());
         } 
-      else if (request.readyState == 4 && request.status != 200) {
-            alert("Something did not work.");
-      }
-
-    };
+        else if (request.readyState == 4 && request.status != 200) {
+          alert("Something did not work.");
+        }
+      };
     }
     );
   }
-
+}
 
 
 function findClosestStop(){
@@ -195,15 +195,15 @@ function findClosestStop(){
   var minDistanceStopIndex = 0;
   for (var i=1; i < tStops.length; i++){
     if (distanceArray[i] < minDistance) {
-        minDistanceStopIndex = i-1; 
-        minDistance = distanceArray[i-1];
+      minDistanceStopIndex = i-1; 
+      minDistance = distanceArray[i-1];
     }
   }
   
   me = new google.maps.Marker({
-        position: me,
-        title: "Current Location. The closest T Stop to you is: " + tStops[minDistanceStopIndex][0] + ". It is: " + minDistance + " miles away from you right now.",
-        map: map
+    position: me,
+    title: "This is your current location.</br> The closest T Stop to you is: " + tStops[minDistanceStopIndex][0] + ".</br> It is: " + minDistance + " miles away from you right now.",
+    map: map
   });
 
   me.addListener('click', function() {
@@ -249,27 +249,7 @@ function linetoNearestStop(stopIndex){
     geodesic: true,
     strokeColor: '#006400',
     strokeOpacity: 3.0,
-     strokeWeight: 5
+    strokeWeight: 5
   });
   nearestStop.setMap(map);
 }
-
-
-
-function getSchedule () {
-  for (var i = 0; i < tStops.length;i++){
-  linkOfRequest = 'https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id='+tStops[i][3];
-  request = new XMLHttpRequest();
-  request.open("GET", linkOfRequest, true);
-  request.send();
-  request.onreadystatechange = function() {
-    if (request.readyState == 4 && request.status == 200) {
-       data = request.responseText;
-       parsedData = JSON.parse(data);
-    }
-    else if (request.readyState == 4 && request.status != 200) {
-            alert("something went wrong");
-        }
-    }
-  }
-  }
